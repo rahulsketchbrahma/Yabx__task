1e1f08f074441b915058989b2a599e3a264fd37c
rahulsketchbrahma <104548244+rahulsketchbrahma@users.noreply.github.com>
2022-06-17 16:34:48 +0530

    Add files via upload

--- src/Components/Stepper/Index.js
+++ src/Components/Stepper/Index.js
@@ -1,50 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Stepper.css";
 
-function Stepper() {
+function Stepper(value) {
+  const InitailColor = {
+    loanSimulator: "greycolor",
+    PreviousLoan: "greycolor",
+    PersonalInfo: "greycolor",
+    Address: "greycolor",
+    Documents: "greycolor",
+    AdditionalInfo: "greycolor",
+    LoanInfo: "greycolor",
+  };
+  const InitailValue = {
+    loanSimulator: "greycolorValue",
+    PreviousLoan: "greycolorValue",
+    PersonalInfo: "greycolorValue",
+    Address: "greycolorValue",
+    Documents: "greycolorValue",
+    AdditionalInfo: "greycolorValue",
+    LoanInfo: "greycolorValue",
+  };
+  const InitailProgressbar = {
+    loanSimulator: "greycolorValue",
+    PreviousLoan: "greycolorValue",
+    PersonalInfo: "greycolorValue",
+    Address: "greycolorValue",
+    Documents: "greycolorValue",
+    AdditionalInfo: "greycolorValue",
+    LoanInfo: "greycolorValue",
+  };
+
+  const [initialColor, setInitialColor] = useState(InitailColor);
+  const [initailValue, setInitialValue] = useState(InitailValue);
+
+  useEffect(() => {
+    if (value.initial === 1) {
+      setInitialColor({ ...initialColor, loanSimulator: "greycolor red" });
+      setInitialValue({
+        ...initailValue,
+        loanSimulator: "greycolorValue redValue",
+      });
+    }
+  }, [value]);
+
   return (
     <div>
       <div className="stepper__wrapper">
         <div className="stepper__container">
           <div className="stepper">
-            <span>1</span>
-            <p>Loan Simulator</p>
+            <span className={`${initialColor.loanSimulator}`}>1</span>
+            <p className={`${initailValue.loanSimulator}`}>Loan Simulator</p>
           </div>
           <div className="stepper">
-            <span>2</span>
-            <p>Loan</p>
+            <span className={`${initialColor.PreviousLoan}`}>2</span>
+            <p className={`${initailValue.PersonalInfo}`}>Previous Loan</p>
           </div>
           <div className="stepper">
-            <span>3</span>
-            <p>Loan</p>
+            <span className={`${initialColor.PersonalInfo}`}>3</span>
+            <p className={`${initailValue.PersonalInfo}`}>Personal Info</p>
           </div>
           <div className="stepper">
-            <span>4</span>
-            <p>Loan</p>
+            <span className={`${initialColor.Address}`}>4</span>
+            <p className={`${initailValue.Address}`}>Address</p>
           </div>
           <div className="stepper">
-            <span>5</span>
-            <p>Loan</p>
+            <span className={`${initialColor.Documents}`}>5</span>
+            <p className={`${initailValue.Documents}`}>Documents</p>
           </div>
           <div className="stepper">
-            <span>6</span>
-            <p>Loan</p>
+            <span className={`${initialColor.AdditionalInfo}`}>6</span>
+            <p className={`${initailValue.AdditionalInfo}`}>Additional Info</p>
           </div>
           <div className="stepper">
-            <span>7</span>
-            <p>Loan</p>
-          </div>
-          <div className="stepper">
-            <span>8</span>
-            <p>Loan</p>
-          </div>
-          <div className="stepper">
-            <span>9</span>
-            <p>Loan</p>
-          </div>
-          <div className="stepper">
-            <span>10</span>
-            <p>Loan</p>
+            <span className={`${initialColor.LoanInfo}`}>7</span>
+            <p className={`${initailValue.LoanInfo}`}>Loan Info</p>
           </div>
         </div>
       </div>
--- src/Container/LoanDetails/Index.js
+++ src/Container/LoanDetails/Index.js
@@ -1,12 +1,194 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./LoanDetails.css";
 import Navbar from "../../Components/Navbar/Index";
 import Stepper from "../../Components/Stepper/Index";
+import { ACTIVE_KYC_ID } from "../../constants";
+import {
+  KYCdefinition,
+  KYCresponsefields,
+} from "../../KYC services/KycServices";
 
 function LoanDetails() {
+  const [rangeValues, setRangeValues] = useState(0);
+  const [rangeYears, setRangeYears] = useState(1);
+  const [RequiredLoanAmmount, setRequiredLoanAmmount] = useState();
+  const minValue = 0;
+  const maxValue = 100000;
+  const minYear = 1;
+  const maxYear = 5;
+
+  //Simple UI functionality
+  const rangeValue = (e) => {
+    setRangeValues(e.target.value);
+  };
+  const rangeYear = (e) => {
+    setRangeYears(e.target.value);
+  };
+
+  //GetKYCdefintion() , Link both url and uuid
+  useEffect(() => {
+    const dataID = {
+      id: ACTIVE_KYC_ID,
+    };
+    KYCdefinition(dataID).then((res) => {
+      const id = res.data.data.packagesDTOs;
+
+      const loanSimulation = id.find((item) => {
+        return item.id === "6225b60783ee70124c8260c0";
+      });
+      const newChildern = loanSimulation.children;
+      const childrenArr = newChildern.map((iteam) => {
+        return {
+          id: iteam.id,
+          fieldName: iteam.fieldName,
+          fieldDisplayName: iteam.fieldDisplayName,
+          mandatory: iteam.mandatory,
+          options: iteam.options,
+          editable: iteam.editable,
+          type: iteam.type,
+        };
+      });
+
+      setRequiredLoanAmmount(childrenArr);
+      console.log(childrenArr);
+    });
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      <Stepper />
+      <div className="loan__simulation__wrapper">
+        <div className="loan__simulation__background">
+          <div>
+            <Navbar />
+          </div>
+          <form>
+            <div className="loan__stepper">
+              <div>
+                <div>
+                  <Stepper initial={1} />
+                </div>
+                <div className="loan__simulator__pls__wrapper">
+                  <div className="loan__simulator__pls">
+                    <div className="loan__simulator__header">
+                      <div className="loan__simulator__header__h3">
+                        <h3>Personal Loan Simulation</h3>
+                      </div>
+                      <div className="loan__simulator__content">
+                        <div className="loan__simulator__content__left">
+                          {RequiredLoanAmmount &&
+                            RequiredLoanAmmount.map((res) => {
+                              if (
+                                res.fieldDisplayName === "Required Loan Amount"
+                              ) {
+                                return (
+                                  <>
+                                    <div className="loan__simulator__content__left__flex">
+                                      <h4>
+                                        {res.fieldDisplayName}
+                                        {res.mandatory ? (
+                                          <span style={{ color: "red" }}>
+                                            *
+                                          </span>
+                                        ) : (
+                                          ""
+                                        )}
+                                      </h4>
+
+                                      <input
+                                        type="number"
+                                        value={rangeValues}
+                                        disabled
+                                      />
+                                    </div>
+                                    <div className="loan__simulator__content__left__range">
+                                      <input
+                                        type="range"
+                                        id="value"
+                                        min={minValue}
+                                        max={maxValue}
+                                        value={rangeValues}
+                                        onChange={rangeValue}
+                                      />
+                                    </div>
+                                    <div className="loan__simulator__content__left__range__values">
+                                      <p>&#8377;{rangeValues}</p>
+                                      <p>&#8377;{maxValue}</p>
+                                    </div>
+                                  </>
+                                );
+                              }
+                              if (
+                                res.fieldDisplayName ===
+                                "Expected Interest Rate"
+                              ) {
+                                return (
+                                  <div>
+                                    <div className="loan__simulator__content__left__EIR">
+                                      <label htmlFor="input">
+                                        {res.fieldDisplayName}
+                                        <input id="input" type="text" />
+                                      </label>
+                                    </div>
+                                  </div>
+                                );
+                              }
+                            })}
+                        </div>
+                        <div className="loan__simulator__content__left">
+                          <div className="loan__simulator__content__left__flex">
+                            <h4>Expected repayment time in months</h4>
+                            <input type="number" value={rangeYears} disabled />
+                          </div>
+                          <div className="loan__simulator__content__left__range">
+                            <input
+                              type="range"
+                              id="year"
+                              min={minYear}
+                              max={maxYear}
+                              value={rangeYears}
+                              onChange={rangeYear}
+                            />
+                          </div>
+                          <div className="loan__simulator__content__left__range__values">
+                            <p>{rangeYears}</p>
+                            <p>{maxYear}Year</p>
+                          </div>
+                          <div>
+                            <div className="loan__simulator__content__left__EIR">
+                              <label htmlFor="input">
+                                Expected EMI ammount
+                                <input id="input" type="text" />
+                              </label>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="loan__simulator__content__left__LP__flex">
+                      <div className="loan__simulator__content__left__LP">
+                        <label htmlFor="input">
+                          Loan Purpose
+                          <select>
+                            <option value="1">
+                              Choose the purpose of loan
+                            </option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                          </select>
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="loanDetails__button__wrapper">
+              <button className="loanDetails__button">continue</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
